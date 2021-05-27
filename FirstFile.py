e1dd2935c7656829785e9f2bb58caff8cb4e21c9
sharanya <sharanyagaddam1111@gmail.com>
2021-05-27 23:32:08 +0530

    Update FirstFile.py

--- FirstFile.py
+++ FirstFile.py
@@ -1,1 +1,5 @@
-
+#Program to calculate radius of circle
+print("Input the radius of the circle:")
+radius=input()
+area=3.14*float(radius)*float(radius)
+print(f"The area of the circle with radius {radius} is:{area}")
